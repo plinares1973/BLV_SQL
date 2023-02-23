a53663808d2b3f427fb5c1d4c14869c813219c12
plinares1973 <116974725+plinares1973@users.noreply.github.com>
2023-02-22 22:24:13 -0300

    Add files via upload

---
+++ TRUNCADOR.sql
@@ -0,0 +1,19 @@
+TRUNCATE TABLE compra.Orden_compra
+TRUNCATE TABLE compra.Orden_compra_producto
+TRUNCATE TABLE produccion.Hoja_fabricacion
+TRUNCATE TABLE Relacion.compra_estado
+TRUNCATE TABLE Relacion.Hoja_fab_estado
+TRUNCATE TABLE Relacion.oc_producto_empaque
+TRUNCATE TABLE Relacion.oc_producto_medida
+TRUNCATE TABLE Relacion.orden_compra_costo_flete
+TRUNCATE TABLE Relacion.orden_compra_envio
+TRUNCATE TABLE Relacion.orden_compra_lote
+TRUNCATE TABLE Relacion.producto_compra
+TRUNCATE TABLE Relacion.producto_hoja_fabrica
+TRUNCATE TABLE Relacion.proveedor_ordencompra
+TRUNCATE TABLE produccion.producto
+TRUNCATE TABLE relacion.lote_producto
+TRUNCATE TABLE relacion.lote_stock
+TRUNCATE TABLE relacion.Orden_Compra_Costo_Flete
+
+
